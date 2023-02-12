147e0c984c1498c3b7b38fcbd467095ec958f601
OuYangMinOa <wesley91345@gmail.com>
2023-02-13 03:23:24 +0800

    nothing change

--- utils/commands.py
+++ utils/commands.py
@@ -5,7 +5,6 @@ from discord.commands import application_command, SlashCommand
 
 TEST_GUILD_ID = loads(getenv("TEST_GUILD_ID"))
 
-
 def slash_command(**kwargs):
     if TEST_GUILD_ID:
         kwargs["guild_ids"] = TEST_GUILD_ID
