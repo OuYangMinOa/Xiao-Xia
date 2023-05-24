b0bfd26df481a47671260262153d311ac65d8149
OuYangMinOa <wesley91345@gmail.com>
2023-05-24 20:42:51 +0800

    add attachment to .gitignore

--- functions/sounds.py
+++ functions/sounds.py
@@ -145,7 +145,7 @@ class BuildSoundSelect():
             if (len(this_select.values)!=0):
                 if (this_select.values[0] in this_label):
                     which_chosen = this_label.index(this_select.values[0])
-
+                    break
         self.sound_class.queqed = [(this_sound[which_chosen],''), ]
         print(self.sound_class.queqed)
         await self.sound_class._next()
