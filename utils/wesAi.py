e4338d27b97b50a1d390679b0123753a3906136b
OuYangMinOa <wesley91345@gmail.com>
2023-07-15 18:37:47 +0800

    add a loop to wait for used address

--- utils/wesAi.py
+++ utils/wesAi.py
@@ -1,13 +1,29 @@
 # on raspi
 from utils.info    import MASSAGE_DATA, PASS_MSG, silinece_channel, logger
+
+import time
 import socket
+import threading
+
+def is_port_in_use(port: int) -> bool:
+    import socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('localhost', port)) == 0
+
 
 def prompt_wes_com(text):   # use my own LLM AI
-    HOST = "192.168.0.2"
+
+    # HOST = "192.168.0.2"
+    HOST = "127.0.0.1"
     PORT = 1564
+
+    while is_port_in_use(PORT):
+        time.sleep(5)
+
+
     try:
         mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        mysocket.settimeout(15)
+        mysocket.settimeout(50)
         mysocket.bind((HOST, PORT))
         mysocket.listen(10)
 
