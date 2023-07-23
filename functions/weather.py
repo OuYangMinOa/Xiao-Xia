d29020b3b856bca6ca31bff8e18b7e9e0904b3a0
OuYangMinOa <wesley91345@gmail.com>
2023-07-23 21:29:54 +0800

    fix and add weather_pos weather_day weather_week

--- functions/weather.py
+++ functions/weather.py
@@ -2,9 +2,10 @@ from discord.commands import slash_command, Option
 from discord.ext      import commands
 from utils.file_os import *
 import discord
+from cairosvg import svg2png
 
 from requests_html import AsyncHTMLSession
-
+import requests
 
 
 class Weather(discord.ext.commands.Cog):
@@ -12,15 +13,20 @@ class Weather(discord.ext.commands.Cog):
         self.bot = bot
 
 
-    @slash_command(name="weather",description="Today's Weather Overview")
-    async def weather(self,ctx):
-        await ctx.respond(f"/weather - {ctx.author.mention}")
+    @slash_command(name="weather_day",description="Today's Weather Overview")
+    async def weather_day(self,ctx):
+        await ctx.respond(f"/weather_day - {ctx.author.mention}")
 
+        
 
         url = "https://www.cwb.gov.tw/V8/C/W/index.html"
         session = AsyncHTMLSession()
         r = await  session.get(url)
         await r.html.arender()
+
+        image_links = ""
+        for i in r.html.xpath("/html/body/div[3]/main/div/div[3]/a"):
+            image_links = 'https://www.cwb.gov.tw'+list(i.links)[0]
 
         text = r.html.xpath("/html/body/div[3]/main/div/div[1]/div/div/div[1]/div")[0].text
         ####################  handle text ####################
@@ -30,9 +36,10 @@ class Weather(discord.ext.commands.Cog):
         for num,each_ch_num in enumerate(ch_num):
             if (each_ch_num in text):
                 text = text.replace(each_ch_num+'、',f"## {each_ch_num}\n - ")
-
+        
         await ctx.send(text)
-
+        
+        await ctx.send(image_links)
         #################### warning message #################
 
         next_text = "\n# :warning:  天氣特報  :warning: \n"
@@ -50,9 +57,9 @@ class Weather(discord.ext.commands.Cog):
         
 
 
-    @slash_command(name="week_weather",description="Weather overview for the week ahead")
-    async def week_weather(self,ctx):
-        await ctx.respond(f"/week_weather - {ctx.author.mention}")
+    @slash_command(name="weather_week",description="Weather overview for the week ahead")
+    async def weather_week(self,ctx):
+        await ctx.respond(f"/weather_week - {ctx.author.mention}")
 
 
         url = "https://www.cwb.gov.tw/V8/C/W/index.html"
@@ -68,6 +75,128 @@ class Weather(discord.ext.commands.Cog):
         text = text + "\n資料來源:中央氣象局"
         await ctx.send(text)
 
+    @slash_command(name="weather_pos",description="Weather overview for the certain position.")
+    async def weather_pos(self,ctx):
+        towns = ["基隆市",
+                "臺北市",
+                "新北市",
+                "桃園市",
+                "新竹市",
+                "新竹縣",
+                "苗栗縣",
+                "臺中市",
+                "彰化縣",
+                "南投縣",
+                "雲林縣",
+                "嘉義市",
+                "嘉義縣",
+                "臺南市",
+                "高雄市",
+                "屏東縣",
+                "宜蘭縣",
+                "花蓮縣",
+                "臺東縣",
+                "澎湖縣",
+                "金門縣",
+                "連江縣"]
+        values = ["10017",
+                "63",
+                "65",
+                "68",
+                "10018",
+                "10004",
+                "10005",
+                "66",
+                "10007",
+                "10008",
+                "10009",
+                "10020",
+                "10010",
+                "67",
+                "64",
+                "10013",
+                "10002",
+                "10015",
+                "10014",
+                "10016",
+                "09020",
+                "09007",]
+        MWS = MyWeatherSelection(ctx,towns,values)
+        await ctx.respond(f"weather_pos {ctx.author.mention}", view=MWS.view, ephemeral=True)
+
+def SvgToPng(url,ouput_filename):
+    re = requests.get(url)
+    svg2png(bytestring=re.content,write_to=ouput_filename)
+    return ouput_filename
+
+
+class MyWeatherSelection:
+    def __init__(self,ctx, towns,values):
+        self.ctx, self.towns, self.values,  = ctx,towns,values
+        options = [ discord.SelectOption(label=towns[i])for i in range(len(towns))]
+
+        self.select = discord.ui.Select(
+                placeholder = "選擇縣市",
+                min_values  = 1, 
+                max_values  = 1,
+                options = options
+            )
+        self.view = discord.ui.View(timeout=24*60*60*7)
+        self.view.add_item(self.select)
+        self.select.callback = self.callback
+    async def callback(self, interaction):
+        which_chosen = self.towns.index(self.select.values[0])
+        if (which_chosen==0):
+            url = "https://www.cwb.gov.tw/V8/C/W/County/index.html"
+        else:
+            url = f"https://www.cwb.gov.tw/V8/C/W/County/County.html?CID={self.values[which_chosen]}"
+
+        await interaction.response.send_message(self.select.values[0])
+
+
+        await self.grabWeatherPositionInformation(url)
+
+    async def grabWeatherPositionInformation(self,url):
+        session = AsyncHTMLSession()
+        r = await  session.get(url)
+        await r.html.arender() 
+        
+        await self.ctx.send("# " + r.html.xpath("/html/body/div[2]/main/div/div[1]/div[1]/div/h2")[0].text)
+        await self.ctx.send("* " + r.html.xpath("/html/body/div[2]/main/div/div[2]/a")[0].text)
+
+        SvgToPng(f"https://www.cwb.gov.tw{r.html.xpath('/html/body/div[2]/main/div/div[1]/div[3]/div[2]/ul/li[1]/img')[0].attrs['src']}",'output1.png')
+        file = discord.File("output1.png")
+        embed1=discord.Embed(title="今晚明晨")
+        embed1.set_thumbnail(url = "attachment://output1.png")
+        embed1.add_field(name="溫度",value=r.html.xpath("/html/body/div[2]/main/div/div[1]/div[3]/div[2]/ul/li[1]/span[2]/span[1]")[0].text,inline=False)
+        embed1.add_field(name=":umbrella: 降雨機率",value=r.html.xpath("/html/body/div[2]/main/div/div[1]/div[3]/div[2]/ul/li[1]/span[3]")[0].text.replace("降雨機率",'\t'),inline=False)
+        embed1.add_field(name="\u200B",value=r.html.xpath("/html/body/div[2]/main/div/div[1]/div[3]/div[2]/ul/li[1]/span[4]")[0].text,inline=False)
+        await self.ctx.send(file=file,embed=embed1)
+
+
+
+        SvgToPng(f"https://www.cwb.gov.tw{r.html.xpath('/html/body/div[2]/main/div/div[1]/div[3]/div[2]/ul/li[2]/img')[0].attrs['src']}",'output2.png')
+        file = discord.File("output2.png")
+        embed2=discord.Embed(title="明日白天")
+        embed2.set_thumbnail(url = "attachment://output2.png")
+        embed2.add_field(name="溫度",value=r.html.xpath("/html/body/div[2]/main/div/div[1]/div[3]/div[2]/ul/li[2]/span[2]/span[1]")[0].text,inline=False)
+        embed2.add_field(name=":umbrella: 降雨機率",value=r.html.xpath("/html/body/div[2]/main/div/div[1]/div[3]/div[2]/ul/li[2]/span[3]")[0].text.replace("降雨機率",'\t'),inline=False)
+        embed2.add_field(name="\u200B",value=r.html.xpath("/html/body/div[2]/main/div/div[1]/div[3]/div[2]/ul/li[2]/span[4]")[0].text,inline=False)
+        await self.ctx.send(file=file,embed=embed2)
+
+
+        SvgToPng(f"https://www.cwb.gov.tw{r.html.xpath('/html/body/div[2]/main/div/div[1]/div[3]/div[2]/ul/li[3]/img')[0].attrs['src']}",'output3.png')
+        file = discord.File("output3.png")
+        embed3=discord.Embed(title="明日晚上")
+        embed3.set_thumbnail(url = "attachment://output3.png")
+        embed3.add_field(name="溫度",value=r.html.xpath("/html/body/div[2]/main/div/div[1]/div[3]/div[2]/ul/li[3]/span[2]/span[1]")[0].text,inline=False)
+        embed3.add_field(name=":umbrella: 降雨機率",value=r.html.xpath("/html/body/div[2]/main/div/div[1]/div[3]/div[2]/ul/li[3]/span[3]")[0].text.replace("降雨機率",'\t'),inline=False)
+        embed3.add_field(name="\u200B",value=r.html.xpath("/html/body/div[2]/main/div/div[1]/div[3]/div[2]/ul/li[3]/span[4]")[0].text,inline=False)
+        await self.ctx.send(file=file,embed=embed3)
+
+
+        await self.ctx.send(f"https://www.cwb.gov.tw{r.html.xpath('/html/body/div[2]/main/div/div[5]/div[1]/div/img')[0].attrs['src']}")
+        
 
 def setup(bot):
     bot.add_cog(Weather(bot))
