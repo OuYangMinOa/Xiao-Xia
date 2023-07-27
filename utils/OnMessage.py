c996bcea8c127e476a42c8f4152994e5041f9764
OuYangMinOa <wesley91345@gmail.com>
2023-07-27 20:32:28 +0800

    Add memory

--- utils/OnMessage.py
+++ utils/OnMessage.py
@@ -39,10 +39,10 @@ async def handle_message(message):
         if ('http' not in PASS_MSG[i] and ('<@' not in PASS_MSG[i]) and  len(re.findall(r'[\u4e00-\u9fff]+',PASS_MSG[i]))>0 ):
             pass_memory_arr.insert(0,PASS_MSG[i])
             count += 1
-        if (len("\n".join(pass_memory_arr)) >400):
+        if (len("\n".join(pass_memory_arr)) >50):
             break
 
-    pass_memory = "\n".join(pass_memory_arr)
+    pass_memory = "\n".join(pass_memory_arr[-4:])
     # print(pass_memory)
     if ("http" not in this_message) :
 
