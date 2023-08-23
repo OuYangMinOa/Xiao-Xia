31f7e50ed3631ae8735bf0fad4678e6113057c95
OuYangMinOa <wesley91345@gmail.com>
2023-08-23 21:45:31 +0800

    Change wesAi from socket to restful api

--- utils/wesAi.py
+++ utils/wesAi.py
@@ -3,6 +3,7 @@ from utils.info    import MASSAGE_DATA, PASS_MSG, silinece_channel, logger
 
 import time
 import socket
+import requests
 import threading
 
 def is_port_in_use(port: int) -> bool:
@@ -30,19 +31,37 @@ def prompt_wes_com(text):   # use my own LLM AI
     # while is_port_in_use(PORT):
     #     time.sleep(5)
 
+    ## Socket way
+    # try:
+    #     mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #     mysocket.settimeout(50)
+    #     mysocket.connect((HOST, PORT))
+    #     mysocket.send(text.encode())
+    #     result = mysocket.recv(4096)
+    #     mysocket.close()
 
-    try:
-        mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        mysocket.settimeout(50)
-        mysocket.connect((HOST, PORT))
-        mysocket.send(text.encode())
-        result = mysocket.recv(4096)
-        mysocket.close()
+    #     return result.decode()
+    # except Exception as e:
+    #     logger.error(e)
+    #     return None
+    
 
-        return result.decode()
-    except Exception as e:
-        logger.error(e)
+    ## change to use restful api
+    prompt = {
+        "promptWord":text,
+        "top_p":0.2,
+        "temperature":1,
+        }
+    response  = requests.post("http://192.168.0.7:8088/prompt",json=prompt)
+    reJson = response.json()
+
+    if (reJson["status"] =="ok"):
+        return reJson["data"]["ouput"]
+    else:
+        logger.error(reJson["status"])
         return None
+
+
 
 
 if __name__ == "__main__":
